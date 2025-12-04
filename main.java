20315b4ee3881f4b50ceb57c6a3a832e8df9a020
Chance <ctkiscool13@gmail.com>
2025-12-03 21:01:08 -0800

    main bugfixes

--- main.java
+++ main.java
@@ -5,6 +5,101 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+class Inserter {
+    public void insertData(Connection conn, String table, String[] values) {
+        String fields = "";
+        try {
+            if (table.equalsIgnoreCase("pet")) {
+                String sql = "INSERT INTO Pet (PetID, ShelterID, Name, Species, Age, Status) VALUES (?, ?, ?, ?, ?, ?)";
+                PreparedStatement ps = conn.prepareStatement(sql);
+                fields = "PetID, ShelterID, Name, Species, Age, Status";
+
+                ps.setInt(1, Integer.parseInt(values[0]));
+                ps.setInt(2, Integer.parseInt(values[1]));
+                ps.setString(3, values[2]);
+                ps.setString(4, values[3]);
+                ps.setInt(5, Integer.parseInt(values[4]));
+                ps.setString(6, values[5]);
+
+                int rowsAffected = ps.executeUpdate();
+                System.out.println(rowsAffected + " row(s) affected. " + Arrays.toString(values));
+            } else if(table.equalsIgnoreCase("medicalhistory")) {
+                String sql = "INSERT INTO MedicalHistory (PetID, HealthStatus, VaccinationStatus) VALUES (?, ?, ?)";
+                PreparedStatement ps = conn.prepareStatement(sql);
+                fields = "PetID, HealthStatus, VaccinationStatus";
+
+                ps.setInt(1, Integer.parseInt(values[0]));
+                ps.setString(2, values[1]);
+                ps.setBoolean(3, Boolean.parseBoolean(values[2]));
+
+                int rowsAffected = ps.executeUpdate();
+                System.out.println(rowsAffected + " row(s) affected. " + Arrays.toString(values));
+            } else if (table.equalsIgnoreCase("adopter")) {
+                String sql = "INSERT INTO Adopter (AdopterID, Name, Number) VALUES (?, ?, ?)";
+                PreparedStatement ps = conn.prepareStatement(sql);
+                fields = "AdopterID, Name, Number";
+
+                ps.setInt(1, Integer.parseInt(values[0]));
+                ps.setString(2, values[1]);
+                ps.setString(3, values[2]);
+
+                int rowsAffected = ps.executeUpdate();
+                System.out.println(rowsAffected + " row(s) affected. " + Arrays.toString(values));
+            } else if (table.equalsIgnoreCase("adoption")) {
+                String sql = "INSERT INTO Adoption (PetID, AdopterID, AdoptionDate) VALUES (?, ?, ?)";
+                PreparedStatement ps = conn.prepareStatement(sql);
+                fields = "PetID, AdopterID, AdoptionDate";
+
+                ps.setInt(1, Integer.parseInt(values[0]));
+                ps.setInt(2, Integer.parseInt(values[1]));
+                ps.setDate(3, Date.valueOf(values[2]));
+
+                int rowsAffected = ps.executeUpdate();
+                System.out.println(rowsAffected + " row(s) affected. " + Arrays.toString(values));
+            } else if (table.equalsIgnoreCase("shelter")) {
+                String sql = "INSERT INTO Shelter (ShelterID, Address) VALUES (?, ?)";
+                PreparedStatement ps = conn.prepareStatement(sql);
+                fields = "ShelterID, Address";
+
+                ps.setInt(1, Integer.parseInt(values[0]));
+                ps.setString(2, values[1]);
+
+                int rowsAffected = ps.executeUpdate();
+                System.out.println(rowsAffected + " row(s) affected. " + Arrays.toString(values));
+            } else if (table.equalsIgnoreCase("foster home")) {
+                String sql = "INSERT INTO FosterHome (FosterID, Address, Number) VALUES (?, ?, ?)";
+                PreparedStatement ps = conn.prepareStatement(sql);
+                fields = "FosterID, Address, Number";
+
+                ps.setInt(1, Integer.parseInt(values[0]));
+                ps.setString(2, values[1]);
+                ps.setString(3, values[2]);
+
+                int rowsAffected = ps.executeUpdate();
+                System.out.println(rowsAffected + " row(s) affected. " + Arrays.toString(values));
+            } else if (table.equalsIgnoreCase("foster assignment")) {
+                String sql = "INSERT INTO FosterAssignemnt (FosterID, PetID, StartDate, EndDate) VALUES (?, ?, ?, ?)";
+                PreparedStatement ps = conn.prepareStatement(sql);
+                fields = "FosterID, PetID, StartDate, EndDate";
+
+                ps.setInt(1, Integer.parseInt(values[0]));
+                ps.setInt(2, Integer.parseInt(values[1]));
+                ps.setDate(3, Date.valueOf(values[2]));
+                ps.setDate(4, Date.valueOf(values[3]));
+
+                int rowsAffected = ps.executeUpdate();
+                System.out.println(rowsAffected + " row(s) affected. " + Arrays.toString(values));
+            } else {
+                System.out.println("Unknown table: " + table);
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL error on insert: " + e.getMessage());
+            System.out.println("For table: " + table + " with values: " + Arrays.toString(values));
+            System.out.println("Try checking formating. Expected fields for " + table + "(in exact order): " + fields);
+        }
+    }
+}
+
 public class main {
     public static void main(String[] args) throws Exception {
         String url = null;
@@ -37,6 +132,7 @@ public class main {
             String input = scnr.nextLine();
 
             if (input.equalsIgnoreCase("view data")) {
+                System.out.println();
                 while (!input.equalsIgnoreCase("back")) {
                     System.out.println("Which data would you like to view?");
                     System.out.println("Tables: \n" + "\t1. Pet \n" + "\t2. MedicalHistory \n" + "\t3. Adopter \n" + "\t4. Adoption \n" + "\t5. Shelter \n" + "\t6. FosterHome \n" + "\t7. FosterAssignment \n");
@@ -134,6 +230,7 @@ public class main {
 
             }
             else if (input.equalsIgnoreCase("insert")) {
+                System.out.println();
                 System.out.println("Please enter the following data for insertion: (Values must be in the exact format as the table schema)");
                 System.out.print("table values[]: ");
                 input = scnr.nextLine();
@@ -146,6 +243,7 @@ public class main {
                 scnr.next();
             }
             else if (input.equalsIgnoreCase("update")) {
+                System.out.println();
                 while (!input.equalsIgnoreCase("back")) {
                     System.out.println("Please enter the following table to update: ");
                     System.out.println("Tables: \n" + "\t1. Pet \n" + "\t2. MedicalHistory \n" + "\t3. Adopter \n" + "\t4. Adoption \n" + "\t5. Shelter \n" + "\t6. FosterHome \n" + "\t7. FosterAssignment \n");
@@ -219,7 +317,7 @@ public class main {
                                 }
                             }
                             else if (update.equalsIgnoreCase("Status")) {
-                                System.out.print("Enter new status as written: (Available, Adopted, Fostered, Unavailable)");
+                                System.out.println("Enter new status as written: (Available, Adopted, Fostered, Unavailable)");
                                 update = scnr.nextLine();
                                 if (update.equalsIgnoreCase("available") || update.equalsIgnoreCase("adopted") || update.equalsIgnoreCase("fostered") || update.equalsIgnoreCase("unavailable")) {
                                     sql = "UPDATE Pet SET Status = ? WHERE PetID = ?";
@@ -266,7 +364,7 @@ public class main {
                             String update = scnr.nextLine();
 
                             if (update.equalsIgnoreCase("HealthStatus")) {
-                                System.out.print("Enter new status as written: (Healthy, Sick, Injured, Chronic Condition");
+                                System.out.println("Enter new status as written: (Healthy, Sick, Injured, Chronic Condition");
                                 update = scnr.nextLine();
                                 if (update.equalsIgnoreCase("Healthy") || update.equalsIgnoreCase("Sick") || update.equalsIgnoreCase("Injured") || update.equalsIgnoreCase("Chronic condition")) {
                                     sql = "UPDATE MedicalHistory SET HealthStatus = ? WHERE PetID = ?";
@@ -283,7 +381,7 @@ public class main {
                                 }
                             }
                             else if (update.equalsIgnoreCase("VaccinationStatus")) {
-                                System.out.print("Enter new status as written: (true, false)");
+                                System.out.println("Enter new status as written: (true, false)");
                                 update = scnr.nextLine();
                                 sql = "UPDATE MedicalHistory SET VaccinationStatus = ? WHERE PetID = ?";
                                 ps = myCon.prepareStatement(sql);
@@ -380,7 +478,7 @@ public class main {
                             String update = scnr.nextLine();
 
                             if (update.equalsIgnoreCase("AdoptionDate")) {
-                                System.out.print("Enter new date: (YYYY-MM-DD)");
+                                System.out.println("Enter new date: (YYYY-MM-DD)");
                                 update = scnr.nextLine();
                                 sql = "UPDATE Adoption SET AdoptionDate = ? WHERE PetID = ? AND AdopterID = ?";
                                 ps = myCon.prepareStatement(sql);
@@ -518,7 +616,7 @@ public class main {
                             String update = scnr.nextLine();
 
                             if (update.equalsIgnoreCase("StartDate")) {
-                                System.out.print("Enter new date: (YYYY-MM-DD)");
+                                System.out.println("Enter new date: (YYYY-MM-DD)");
                                 update = scnr.nextLine();
                                 sql = "UPDATE FosterAssignment SET StartDate = ? WHERE FosterID = ? AND PetID = ?";
                                 ps = myCon.prepareStatement(sql);
@@ -531,7 +629,7 @@ public class main {
                                 }
                             }
                             else if (update.equalsIgnoreCase("EndDate")) {
-                                System.out.print("Enter new date: (YYYY-MM-DD)");
+                                System.out.println("Enter new date: (YYYY-MM-DD)");
                                 update = scnr.nextLine();
                                 sql = "UPDATE FosterAssignment SET EndDate = ? WHERE FosterID = ? AND PetID = ?";
                                 ps = myCon.prepareStatement(sql);
@@ -551,6 +649,7 @@ public class main {
                 }
             }
             else if (input.equalsIgnoreCase("delete")) {
+                System.out.println();
                 System.out.println("Please enter the following data for delete: ");
                 System.out.print("Table: ");
                 input = scnr.nextLine();
@@ -635,6 +734,7 @@ public class main {
                 }
             }
             else if (input.equalsIgnoreCase("run transaction")) {
+                System.out.println();
                 Connection myCon = null;
                 try {
                     myCon = DriverManager.getConnection(url, username, password);
